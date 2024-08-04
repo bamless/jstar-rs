cd3b7c50e317eaccc95eaf5fe176bd56fdba7f99
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2024-08-04 14:05:12 +0200

    Update `native!` example

--- src/native.rs
+++ src/native.rs
@@ -12,18 +12,26 @@ pub const MIN_NATIVE_STACK_SZ: usize = crate::ffi::JSTAR_MIN_NATIVE_STACK_SZ;
 /// # Example
 ///
 /// ```
-/// # use jstar::{native, vm::VM, conf::Conf, MAIN_MODULE};
+/// # use jstar::{native, vm::VM, conf::Conf, MAIN_MODULE, convert::{ToJStar, FromJStar}};
 /// # let mut vm = VM::new(Conf::new()).init_runtime();
 /// // The `vm` argument is a mutable reference to the J* VM (&mut VM).
-/// native!(fn nativeFn(vm) {
-///     // Your code here
-///     // ...
+/// native!(fn rustAdd(vm) {
+///     // `from_jstar_checked` returns an `Err(crate::error::Error::Runtime)` if
+///     // the value at the given stack slot is not an i32.
+///     let a = i32::from_jstar_checked(vm, 1, "a")?;
+///     let b = i32::from_jstar_checked(vm, 2, "b")?;
+///
+///     (a + b).to_jstar(vm);
+///
 ///     // Return a `Result<(), crate::error::Error>`
 ///     Ok(())
 /// });
-/// # vm.register_native(MAIN_MODULE, "nativeFn", nativeFn, 0);
-/// # vm.get_global(MAIN_MODULE, "nativeFn").unwrap();
-/// # vm.call(0).unwrap();
+/// # vm.register_native(MAIN_MODULE, "rustAdd", rustAdd, 2);
+/// # vm.get_global(MAIN_MODULE, "rustAdd").unwrap();
+/// # 1.to_jstar(&vm);
+/// # 2.to_jstar(&vm);
+/// # vm.call(2).unwrap();
+/// # assert_eq!(i32::from_jstar(&vm, -1).unwrap(), 3);
 /// ```
 #[macro_export]
 macro_rules! native {
